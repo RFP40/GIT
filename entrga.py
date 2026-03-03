2111139c05c32f8472c1f08f8e972c16422ce454
Rafa <rafamesto@gmail.com>
2026-03-03 09:36:35 -0300

    Código.py

---
+++ entrga.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+conexao = sqlite3.connect("banco_de_dados.db")
+cursor = conexao.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS pecas(
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                cpf TEXT NOT NULL UNIQUE,
+                local TEXT NOT NULL,
+                cep INT NOT NULL)""")
