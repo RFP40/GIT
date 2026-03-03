c325f6b5646654dcb80a27328d43e89ad67cc34c
Rafa <rafamesto@gmail.com>
2026-03-03 08:16:48 -0300

    Criação do BDD, adição dos primeiros dados!

---
+++ main.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conexao = sqlite3.connect("banco_de_dados.db")
+cursor = conexao.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS teste(
+               id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+               nome TEXT NOT NULL,
+               cpf TEXT NOT NULL UNIQUE
+               )""")
+
+cursor.execute("""INSERT INTO teste
+               (nome, cpf) VALUES
+               ('Rafael', '506135898-82' )""")
+
+conexao.commit()
