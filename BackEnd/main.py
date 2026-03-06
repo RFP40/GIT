f25c652f28a9300c10f8e6440bf0e557bf61ecce
Rafael Mesquita Torres <rafamesto@gmail.com>
2026-03-06 10:30:27 -0300

    Update main.py

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -9,7 +9,7 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS login_site(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE, 
     senha TEXT NOT NULL
 )
 """)
